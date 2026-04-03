406f827547bbde8582fe9d5a361eaec464f3d5bd
Swaleha <swalehapatwekar16@gmail.com>
2026-04-03 19:51:25 +0530

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,4 @@
 // src/apiConfig.js
-const API_BASE_URL = "https://nexhealth-backend.onrender.com"; 
+const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://nexhealth-backend.onrender.com"; 
 
 export default API_BASE_URL;
